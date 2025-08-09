fcb3a7b8f068cce03eb29466f2ab68f93d4b177f
Abdur Raheem Nauman <abdurraheem.geek@gmail.com>
2025-08-09 09:36:13 +0500

    Edit server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
+import postRoutes from './routes/post.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -22,6 +23,8 @@ app.use(cookieParser());
 
 //Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
 
+//Server listening
 app.listen(PORT || 5000, () => { console.log(`Server is started at PORT:${PORT}`) });
 
